327c4d5668c3cc9ef96a0e2382eefb88ad271680
Ihor <mrshevchenko33@gmail.com>
2024-02-29 21:29:55 +0200

    change #

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ my_string = input('Enter your hashtag: ')
 my_lst = my_string.split()
 my_lst = [''.join(i for i in word if i not in string.punctuation) for word in my_lst]
 
-new_string = ''.join('#' + word.capitalize() for word in my_lst if word)
+new_string = ''.join(word.capitalize() for word in my_lst if word)
 
 if len(new_string) > 140:
     new_string = new_string[:139]
 
-print(new_string)
+print('#' + new_string)
 
 
 
